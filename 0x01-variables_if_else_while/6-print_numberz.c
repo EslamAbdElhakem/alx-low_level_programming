c7d11f8012a86d1b8d8d1a0fc90a169923c2a74f
EslamAbdElhakem <eslam_hakem@ymail.com>
2023-02-20 23:59:42 +0200

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+/* headers */
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 0;
+	while (num <= 9)
+	{
+		/*convert digit to ASCII */
+		putchar(num + '0');
+		++num;
+	}
+	putchar('\n');
+	return (0);
+}
